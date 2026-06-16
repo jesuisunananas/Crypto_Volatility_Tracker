7acbfac20e92ae623e666e1f12499426ed925541
Arjun Rewari <arjun.mahay@gmail.com>
2026-06-15 19:51:56 -0700

    added initialize scaler function from historical data

--- app.py
+++ app.py
@@ -5,15 +5,44 @@ import json
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import torch
+import torch.optim as optim
+import torch.nn as nn
 from model import VolatilityPredictor
+import requests
 
-scaler = StandardScaler()
 model = VolatilityPredictor(input_size=2, hidden_size=16, num_layers=2)
-model.eval()
+model.train()
 
-async def stream_data(k=100):
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.BCELoss()
+
+def initialize_scaler(symbol="BTCUSDT", limit=1000):
+    url = f"https://api.binance.us/api/v3/klines?symbol={symbol}&interval=1m&limit={limit}"
+    response = requests.get(url).json()
+    prev_features = []
+    
+    for kline in response:
+        price_high = float(kline[2])
+        price_low = float(kline[3])
+        price_close = float(kline[4])
+        volume = float(kline([5]))
+        prev_features.append([(price_high - price_low)/price_close, volume])
+
+    arr = np.array(prev_features)
+    scaler = StandardScaler()
+    scaler.fit(arr)
+    return scaler
+
+async def stream_data(scaler=None, k=100):
+    if not scaler:
+        print("Error: Scaler must be initialized first with initalize_scaler()")
+        return
+
     uri = "wss://stream.binance.us:9443/ws/btcusdt@kline_1m"
     window = deque(maxlen=k)
+    pending_sample = None
+    VOLATILITY_THRESHOLD = 0.002
+
     async with websockets.connect(uri, ssl=True) as websocket:
         print("Connected to Binance Stream")
         try:
@@ -25,10 +54,11 @@ async def stream_data(k=100):
                 price_close = float(kline['c'])
                 volume = float(kline['v'])
                 is_closed = kline['x']
+                candle_start_time = kline['t']
 
                 if is_closed and volume > 0.0:
                     print(f"Candle Closed | Close: ${price_close} | High: ${price_high} | Low: ${price_low} | Vol: {volume}")
-                    new_features = [price_high - price_low, volume]
+                    new_features = [(price_high - price_low) / price_close, volume]
                     window.append(new_features)
                     if len(window) >= k:
                         arr = np.array(window)
@@ -45,4 +75,5 @@ async def stream_data(k=100):
             print("Connection to Binance Stream closed")
 
 if __name__ == "__main__":
-    asyncio.run(stream_data())
+    scaler = initialize_scaler()
+    asyncio.run(stream_data(scaler))
